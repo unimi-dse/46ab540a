a6b982a8ad6b47dc3f92c18c21c07b295005db58
NGUYEN QA <59968046+jyanqa@users.noreply.github.com>
2020-02-08 08:06:36 +0100

    Add files via upload

--- R/Sentimental analysis.R
+++ R/Sentimental analysis.R
@@ -6,7 +6,7 @@ GetSentiment <- function(product_code,page_range){
   sentiment <- tokens %>% dplyr::inner_join(tidytext::get_sentiments("bing")) %>%
     dplyr::count(sentiment) %>%
     tidyr::spread(sentiment, n, fill = 0) %>%
-    dplyr::mutate(sentiment = 3*negative-2*positive) #check if [x/(x+y)]>40%
+    dplyr::mutate(sentiment = 3*negative-2*positive) #check if x/(x+y)]>40%
   a <- sentiment [[3]]
   if (a<0){message("This product recieved good reviews")} else {message("BE CAREFUL! This product recieved BAD reviews")}
   return(sentiment) #if number of negative words is greater than 40% of total sentimental words then that product is considered as 'got BAD reviews'.
